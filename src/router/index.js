4d6f16f1a33c7bfab4f94040f01da46e3483d614
liaofan <wangyushan6@163.com>
2018-06-08 10:57:46 +0800

    更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ShoppingMall from '@/components/pages/ShoppingMall'
-
+import ShoppingMall from '@/components/pages/ShoppingMall';
+import Register from '@/components/pages/Register';
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'ShoppingMall',
       component: ShoppingMall
+    },{
+      path: '/register',
+      name: 'Register',
+      component: Register
     }
+
   ]
 })
